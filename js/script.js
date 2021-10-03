be28a4292f85a6d1e0048a84cf682e5c2fae882c
kareemZain2 <batkare191@gmail.com>
2021-10-03 20:27:32 +0300

    2

--- js/script.js
+++ js/script.js
@@ -11,6 +11,12 @@ $(function (){
 
     var homeHtml = "snippets/home-snippet.html";
 
+    var allCategoriesHtml = "http://davids-restaurant.herokuapp.com/categories.json";
+
+    var categoriesTitleHtml = "snippets/categories-title-sippet.html";
+
+    var categoryHtml = "snippets/category-snippet.html";
+
     var insertHtml = function(selector,html){
         var targetElem = document.querySelector(selector);
         targetElem.innerHTML = html;
@@ -22,11 +28,45 @@ $(function (){
         insertHtml(selector,html);
     };
 
+    var insertProperty = function(string,propName,propValue){
+        var propToReplace = "{{" + propName + "}}";
+        string = string.replace(new RegExp(propToReplace,"g"),propValue);
+        return string;
+    }
+
     document.addEventListener("DOMContentLoaded",function(event){
         showLoading("#main-content");
         $ajaxUtils.sendGetRequest(homeHtml,function(responseText){
             document.querySelector("#main-content").innerHTML = responseText;
         },false);
+
+        dc.loadMenuCategories = function(){
+            showLoading("#main-content");
+            $ajaxUtils.sendGetRequest(allCategoriesHtml,buildAndShowCategoriesHtml);
+        };
+        function buildAndShowCategoriesHtml(categories){
+            $ajaxUtils.sendGetRequest(categoriesTitleHtml,function(categoriesTitleHtml){
+                $ajaxUtils.sendGetRequest(categoryHtml,function(categoryHtml){
+                    var categoriesviewHtml = buildCategoriesViewHtml(categories,categoriesTitleHtml,categoryHtml);
+                    insertHtml("#main-content",categoriesviewHtml);
+                },false);
+            },false);
+        };
+        
+        function buildCategoriesViewHtml(categories,categoriesTitleHtml,categoryHtml){
+            var finalHtml = categoriesTitleHtml;
+            finalHtml += "<section class='row'>";
+            for(var i = 0;i < categories.length;i++){
+                var html = categoryHtml;
+                var name = "" + categories[i].name;
+                var short_name = categories[i].short_name;
+                html = insertProperty(html,"name",name);
+                html = insertProperty(html,"short_name",short_name);
+                finalHtml += html;
+            }
+            finalHtml += "</section>";
+            return finalHtml;
+        };
     });
     global.$dc = dc;
 })(window);
